3d8c9b5f02fb19a5fbf7e9980adf60790214b677
BLR <bruff@Macky-Max.local>
2024-11-30 22:25:29 -0500

    this commit Defines the initial bounds.

--- main.java
+++ main.java
@@ -2,3 +2,23 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
+import org.w3c.dom.Node;
+
+/**
+ * A Quadtree data structure 
+ * that allows for the insertion, deletion, 
+ * updating, and retrieval of rectangles 
+ * in a 2D space.
+ */
+
+public class main {
+    private Node root;
+
+    /**
+     * Defines the initial bounds.
+     */
+    public main() {
+        this.root = new LeafNode(-50, -50, 100, 100);
+    }
+}
